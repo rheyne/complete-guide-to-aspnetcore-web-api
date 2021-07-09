212c611ec1dc71194171a2cf83bbde3da794209c
René Heyne <rene.heyne@relaxdays.de>
2021-07-09 13:27:06 +0200

    Part 11 Seeding Your Database With Data

---
+++ my-books/Data/AppDbInitializer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using my_books.Data.Models;
+
+namespace my_books.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                if (!context.Books.Any())
+                {
+                    var firstBook = new Book()
+                    {
+                        Title = "1st Book Title",
+                        Description = "1st Book Description",
+                        IsRead = true,
+                        DateRead = DateTime.Now.AddDays(-10),
+                        Rate = 4,
+                        Genre = "Biography",
+                        Author = "First Author",
+                        CoverUrl = "https...",
+                        DateAdded = DateTime.Now
+                    };
+
+                    var secondBook = new Book()
+                    {
+                        Title = "2nd Book Title",
+                        Description = "2nd Book Description",
+                        IsRead = false,
+                        Genre = "Biography",
+                        Author = "First Author",
+                        CoverUrl = "https...",
+                        DateAdded = DateTime.Now
+                    };
+                    context.Books.AddRange(firstBook, secondBook);
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}

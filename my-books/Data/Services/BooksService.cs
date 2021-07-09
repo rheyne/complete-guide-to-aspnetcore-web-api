afd7c4f210177acaf5423527895f171585c890e2
René Heyne <rene.heyne@relaxdays.de>
2021-07-09 14:38:55 +0200

    Part 16 Delete an Existing Book [HttpDelete]

--- my-books/Data/Services/BooksService.cs
+++ my-books/Data/Services/BooksService.cs
@@ -57,5 +57,15 @@ namespace my_books.Data.Services
 
             return _book;
         }
+
+        public void DeleteBookById(int bookId)
+        {
+            var _book = _context.Books.FirstOrDefault(b => b.Id == bookId);
+            if (_book != null)
+            {
+                _context.Books.Remove(_book);
+                _context.SaveChanges();
+            }
+        }
     }
 }

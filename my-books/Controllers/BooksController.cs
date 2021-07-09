9516b7a11beeb3d51c6dc0be3de5af93e181d734
René Heyne <rene.heyne@relaxdays.de>
2021-07-09 13:49:36 +0200

    Part 12 and 13 Adding Your First Web API Controller and Adding a New Book [HttpPost]

---
+++ my-books/Controllers/BooksController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using my_books.Data.ViewModels;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BooksService _booksService;
+
+        public BooksController(BooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody]BookVM book)
+        {
+            _booksService.AddBook(book);
+            return Ok();
+        }
+    }
+}
---
+++ my-books/Data/Services/BooksService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using my_books.Data.Models;
+using my_books.Data.ViewModels;
+
+namespace my_books.Data.Services
+{
+    public class BooksService
+    {
+        private AppDbContext _context;
+
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook(BookVM book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead : null,
+                Rate = book.Rate,
+                Genre = book.Genre,
+                Author = book.Author,
+                CoverUrl = book.CoverUrl,
+                DateAdded = DateTime.Now
+            };
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+    }
+}

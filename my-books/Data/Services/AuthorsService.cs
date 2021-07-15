13c4e046fff1a2e279625301be6fa718370ff29f
René Heyne <rene.heyne@relaxdays.de>
2021-07-15 14:46:54 +0200

    24. Getting Books of an Author with [HttpGet]

--- my-books/Data/Services/AuthorsService.cs
+++ my-books/Data/Services/AuthorsService.cs
@@ -1,5 +1,6 @@
 ﻿using my_books.Data.Models;
 using my_books.Data.ViewModels;
+using System.Linq;
 
 namespace my_books.Data.Services
 {
@@ -20,6 +21,17 @@ namespace my_books.Data.Services
             };
             _context.Authors.Add(_author);
             _context.SaveChanges();
+        }
+
+        public AuthorWithBooksVM GetAuthorWithBooks(int authorId)
+        {
+            var _author = _context.Authors.Where(n => n.Id == authorId).Select(n => new AuthorWithBooksVM()
+            {
+                FullName = n.FullName,
+                BookTitles = n.Book_Authors.Select(n => n.Book.Title).ToList()
+            }).FirstOrDefault();
+
+            return _author;
         }
     }
 }

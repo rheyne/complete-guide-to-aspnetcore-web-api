002f712b529a2d114837c2c3ed9dfd8d3df5d90a
René Heyne <rene.heyne@relaxdays.de>
2021-07-16 09:58:19 +0200

    32. Custom Return Type (Controller Result Type)

--- my-books/Controllers/PublishersController.cs
+++ my-books/Controllers/PublishersController.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using my_books.Data.Models;
+using my_books.ActionResults;
 using my_books.Data.Services;
 using my_books.Data.ViewModels;
+using System;
 
 namespace my_books.Controllers
 {
@@ -24,17 +25,32 @@ namespace my_books.Controllers
         }
 
         [HttpGet("get-publisher-by-id/{id}")]
-        public ActionResult<Publisher> GetPublisherById(int id)
+        public CustomActionResult GetPublisherById(int id)
         {
             var _response = _publishersService.GetPublisherById(id);
             if (_response != null)
             {
                 //return Ok(_response);
-                return _response;
+
+                var _responseObj = new CustomActionResultVM()
+                {
+                    Publisher = _response
+                };
+
+                return new CustomActionResult(_responseObj);
+
+                //return _response;
             }
             else
             {
-                return NotFound();
+                var _responseObj = new CustomActionResultVM()
+                {
+                    Exception = new Exception("This is coming from publishers controller")
+                };
+
+                return new CustomActionResult(_responseObj);
+
+                //return NotFound();
             }
         }
 

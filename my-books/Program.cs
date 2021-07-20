5784a922162ad66edbc4626b5c81fb964970c851
René Heyne <rene.heyne@relaxdays.de>
2021-07-20 13:22:45 +0200

    Abschnitt 7: Web API Logging with Serilog - 48. Setting up Serilog in Web API

--- my-books/Program.cs
+++ my-books/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Serilog;
 
 namespace my_books
 {
@@ -13,11 +8,21 @@ namespace my_books
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                Log.Logger = new LoggerConfiguration().CreateLogger();
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();

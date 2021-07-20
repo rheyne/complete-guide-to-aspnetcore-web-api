14eef5e6a4e33b27f8c273513f1ceba91ac8e94e
René Heyne <rene.heyne@relaxdays.de>
2021-07-20 13:59:34 +0200

    Abschnitt 7: Web API Logging with Serilog - 50. Adding a new Log table using EFCore migrations

--- my-books/Data/AppDbContext.cs
+++ my-books/Data/AppDbContext.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using my_books.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace my_books.Data
 {
@@ -25,6 +21,8 @@ namespace my_books.Data
                 .WithMany(ba => ba.Book_Authors)
                 .HasForeignKey(bi => bi.AuthorId);
 
+            modelBuilder.Entity<Log>().HasKey(n => n.Id);
+
             base.OnModelCreating(modelBuilder);
         }
 
@@ -32,5 +30,6 @@ namespace my_books.Data
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book_Author> Books_Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Log> Logs { get; set; }
     }
 }
---
+++ my-books/Migrations/20210720115753_LogTableAdded.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace my_books.Migrations
+{
+    // Created via NuGet-Paket-Manager -> Paket-Manager-Konsole -> "Add-Migration LogTableAdded"
+    // Executed via NuGet-Paket-Manager -> Paket-Manager-Konsole -> "Update-Database"
+    public partial class LogTableAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Message = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    MessageTemplate = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Level = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TimeStamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Exception = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Properties = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LogEvent = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logs");
+        }
+    }
+}

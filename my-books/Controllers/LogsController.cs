eea6faac23046bb495c991b5093c34c0e6f557e0
René Heyne <rene.heyne@relaxdays.de>
2021-07-21 15:44:22 +0200

    Abschnitt 7: Web API Logging with Serilog - 52. Retreiving all logs from the database

---
+++ my-books/Controllers/LogsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using System;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogsController : ControllerBase
+    {
+        private LogsService _logsService;
+
+        public LogsController(LogsService logsService)
+        {
+            _logsService = logsService;
+        }
+
+        [HttpGet("get-all-logs-from-db")]
+        public IActionResult GetAllLogsFromDB()
+        {
+            try
+            {
+                var allLogs = _logsService.GetAllLogsFromDB();
+                return Ok(allLogs);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Could not load logs from the database");
+            }
+        }
+    }
+}
